0f965414b96735c20865a5aa6e8830e02df18afc
Stjepan Kos <sk52440@fer.hr>
2022-11-11 15:44:26 +0100

    added connection timeout to client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,23 @@
 use std::io::{self, Read, Write};
-use std::net::TcpStream;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream};
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
-const SERVER_ADDR: &str = "127.0.0.1:8000";
+//const SERVER_ADDR: &str = "127.0.0.1:8000";
 const MSG_SIZE: usize = 32;
+
 fn main() {
-    let mut reader_stream =
-        TcpStream::connect(SERVER_ADDR).expect("Could not connect to the server.");
+    let mut reader_stream = match TcpStream::connect_timeout(
+        &SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8000),
+        Duration::from_secs(5),
+    ) {
+        Ok(stream) => stream,
+        Err(_) => {
+            println!("Error connecting to the server");
+            return;
+        }
+    };
     let mut writer_stream = reader_stream.try_clone().unwrap();
 
     let (tx, rx) = mpsc::channel::<String>();
@@ -17,6 +27,7 @@ fn main() {
         match reader_stream.read_exact(&mut buf) {
             Ok(_) => {
                 let message = buf.into_iter().take_while(|&x| x != 0).collect();
+                println!("new message incoming");
                 match String::from_utf8(message) {
                     Ok(message) => println!("{}", message),
                     Err(_) => println!("Error converting the message to utf-8"),
